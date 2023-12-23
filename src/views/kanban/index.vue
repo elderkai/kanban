799937772fb11e3f685ba22293ed8f5a9dffbc28
elderkai <2498695596@qq.com>
2023-12-24 00:00:20 +0800

    优化添加卡片效果

--- src/views/kanban/index.vue
+++ src/views/kanban/index.vue
@@ -2,40 +2,52 @@
   <div class="baseKanban">
     <div class="hedader">
       <div class="cover">
-        <el-input v-model="searchText" :prefix-icon="Search" class="searchInput" placeholder="搜索相关内容" @change="filterText">
+        <el-input v-model="searchText" :prefix-icon="Search" class="searchInput" placeholder="搜索相关内容"
+          @change="filterText">
         </el-input>
       </div>
     </div>
     <div class="box_cover">
       <div class="list_todo" v-for="(item, key) in myTodos" :key="key">
         <div class="todo_top add_btn_top" v-if="item.type" @click="addList">
-          <div class="title">{{ item.title }}</div>
+          <div class="title">{{ item.title}}
+        </div>
         </div>
         <div class="todo_top" v-else>
-          <div class="title">{{ item.title }}</div>
+          <div class="title">{{ item.title }}（<span v-if="item.children">
+            {{  item.children.length+1 }}）
+            </span></div>
         </div>
-        <draggable v-if="item.children" class="dragArea list-group" :drag-class="'dragClass'" :force-fallback="true" :list="item.children"
-          :clone="clone" ghost-class="ghost" chosen-class="chosenClass" animation="300"  :handle="`.mover`" 
-          :group="{ name: 'dragArea', pull: pullFunction }" @start="start" @end="onEnd" :move="onMove" item-key="id">
+        <draggable v-if="item.children" class="dragArea list-group" :drag-class="'dragClass'" :force-fallback="true"
+          :list="item.children" :clone="clone" ghost-class="ghost" chosen-class="chosenClass" animation="300"
+          :handle="`.mover`" :group="{ name: 'dragArea', pull: pullFunction }" @start="start" @end="onEnd" :move="onMove"
+          item-key="id">
           <template #item="{ element }">
-            <div v-if="element.type=='add'" class="todo_item" >
-              <div class="add_text" @click.stop="addTodo(item)">
+            <div v-if="element.type == 'add'" class="todo_item">
+              <div v-show="element.showInput">
+                <el-input class="no_border" v-model="element.inputText" ref="kbInput" placeholder="输入卡片名称" @blur="blurInput" resize="none" :autosize="{ minRows: 2, maxRows: 4 }" type="textarea"> </el-input>
+              </div>
+              <div v-if="!element.showInput" class="add_text" @click.stop="addTodo(key,element)">
                 +添加卡片
               </div>
-              </div>
-            <div :class="`todo_item ${element.move?'mover':''}`" v-else-if="element.type=='todo'">
+            </div>
+            <div :class="`todo_item ${element.move ? 'mover' : ''}`" @click="editKb(key,element)" v-else-if="element.type == 'todo'" v-show="searchText==''||element.title.indexOf(searchText)>-1">
               {{ element.title }}</div>
           </template>
         </draggable>
       </div>
     </div>
+    <mDialog ref="dialogVue"></mDialog>
   </div>
 </template>
 
 <script lang="ts" setup>
+import mDialog from './m-dialog.vue';
 import { Search } from '@element-plus/icons-vue'
 import draggable from "vuedraggable";
 import { ref } from "vue";
+let fourKey:any = 0;
+let kbInput = ref(),dialogVue = ref();
 let searchText = ref('');
 let myTodos: any = ref([
   {
@@ -60,9 +72,9 @@ let myTodos: any = ref([
   },
 ]);
 let isMove = ref(false);
-function filterText(val:any){
+function filterText(val: any) {
   console.log(val);
-  
+
 }
 function init() {
   let key = 1;
@@ -70,72 +82,100 @@ function init() {
     for (let index = 0; index < 10; index++) {
       it.children.push({
         title: `${it.title}--${index + 1}`,
-        type:'todo',
-        move:true,
+        type: 'todo',
+        move: true,
         key
       })
       key++
     }
     it.children.push({
-        title: ``,
-        type:'add',
-        move:false,
-        key
-      })
-      key++
+      title: ``,
+      type: 'add',
+      move: false,
+      showInput: false,
+      inputText:'',
+      key
+    })
+    key++
   });
-  myTodos.value.push({title:"+添加列表",type:'add'})
+  myTodos.value.push({ title: "+添加列表", type: 'add' })
   console.log(myTodos.value);
 
 }
 init()
+function blurInput(val:any){
+  console.log(val);
+  let list =  myTodos.value[fourKey].children
+
+  if(list[list.length-1].inputText){
+    console.log(111);
+    list.splice(list.length - 1, 0, { title: list[list.length-1].inputText, type: "todo", move: true, })
+    list[list.length-1].inputText = ''
+    
+  }
+  list[list.length-1].showInput = !list[list.length-1].showInput
+  
+}
 function clone(val: any) {
   console.log(val);
   return val
 
 
 }
+function editKb(key:any,ele:any){
+  console.log(key,ele);
+  dialogVue.value.dialogVisible = true
+  
+
+}
 function onMove(val: any) {
-  if(!val.relatedContext.list[val.draggedContext.futureIndex]){
+  if (!val.relatedContext.list[val.draggedContext.futureIndex]) {
     return false
   }
-  if(val.relatedContext.list[val.draggedContext.futureIndex].type=='add'){
+  if (val.relatedContext.list[val.draggedContext.futureIndex].type == 'add') {
     return false;
   }
-  if(val.draggedContext.element.type=='add'){
+  if (val.draggedContext.element.type == 'add') {
     return false
   }
   console.log(111);
-  
+
   return val
 
 
 }
 function start(val: any) {
-  console.log('start',val);
+  console.log('start', val);
   isMove.value = true;
 
   return val
 }
 function onEnd(val: any) {
   isMove.value = false;
-  console.log('onEnd',val);
+  console.log('onEnd', val);
 
   return val
 }
-function pullFunction(val:any) {
-  console.log('pullFunc',val);
+function pullFunction(val: any) {
+  console.log('pullFunc', val);
 
-// if(val.type=='add'){
-//   return false
-// }
+  // if(val.type=='add'){
+  //   return false
+  // }
   return true;
 }
-function addTodo(val:any){
-  val.children.splice(val.children.length-1,0,{title:'',type:"todo",   move:true,})
+function addTodo(key:any,it:any) {
+  it.showInput = true;
+  fourKey = key;
+  setTimeout(() => {
+    console.log(kbInput.value[key].focus(),it);
+    
+  }, 100);
+  
+  // val.children.splice(val.children.length - 1, 0, { title: '', type: "todo", move: true, })
 }
-function addList(){
-alert('请稍后')
+function addList() {
+  alert('请稍后')
 }
 </script>
 
@@ -215,35 +255,42 @@ alert('请稍后')
   }
 
   .list_todo {
-    margin-top: 20px;
-    padding: 0 20px;
+    margin-top: 10px;
     height: 100%;
     min-width: 220px;
     position: relative;
     height: 100%;
-    .list-group{
+    border-radius: 15px;
+    padding-top: 20px;
+
+    &:hover {
+      background: #051f26;
+    }
+
+    .list-group {
       height: calc(100vh - 150px);
       max-height: calc(100vh - 150px);
       overflow-y: auto;
+      padding: 0 20px;
       &::-webkit-scrollbar {
         width: 10px;
       }
-  
+
       &::-webkit-scrollbar-track {
         background: #0c3540;
         border-radius: 20px;
       }
-  
+
       &::-webkit-scrollbar-thumb {
         background: #6ac1cd;
         border-radius: 10px;
       }
-  
+
       &::-webkit-scrollbar-thumb:hover {
         background: #6ac1cd;
         border-radius: 20px;
       }
-  
+
       &::-webkit-scrollbar-thumb:active {
         background: #6ac1cd;
         border-radius: 20px;
@@ -252,33 +299,38 @@ alert('请稍后')
   }
 
   .todo_top {
-    background: #52bfd1;
-    padding: 10px;
+    // background: #52bfd1;
+    // padding: 20px;
     border-radius: 50px;
     margin-bottom: 20px;
-    text-align: center;
+    text-align: left;
     box-sizing: border-box;
-    box-shadow: 11px 9px 11px rgba(69, 67, 67, 0.2);
+    color: #fff;
+    padding-left: 20px;
+    // box-shadow: 11px 9px 11px rgba(69, 67, 67, 0.2);
   }
 
   .todo_item {
     position: relative;
-    background: #fff4ec;
+    background: #fff;
     padding: 10px;
-    border-radius: 15px;
+    border-radius: 10px;
     margin-bottom: 10px;
-    width: max-content;
-    min-width: 120px;
-    width: 100%;
+    width: 210px;
+    max-width: 210px;
     box-sizing: border-box;
     box-shadow: 11px 9px 11px rgba(69, 67, 67, 0.2);
-    cursor: all-scroll;
+    cursor: pointer;
     min-height: 32px;
+  }
+  .mover{
+    min-height: 49px;
   }
 }
 
 .chosenClass {
   background-color: #fff;
+  
 }
 
 .ghost {
@@ -292,9 +344,11 @@ alert('请稍后')
 .dragClass {
   background: #fff !important;
   opacity: 1 !important;
-  transform:rotate(2deg)
+  transform: rotate(3deg);
+   cursor: all-scroll!important;
 }
-.todo_btn_add{
+
+.todo_btn_add {
   background: #FFF;
   border-radius: 15px;
   height: 32px;
@@ -302,7 +356,8 @@ alert('请稍后')
   text-align: center;
   cursor: pointer;
 }
-.add_text{
+
+.add_text {
   position: absolute;
   width: 100%;
   height: 100%;
@@ -316,10 +371,14 @@ alert('请稍后')
 }
 
 
-.box_cover .add_btn_top{
+.box_cover .add_btn_top {
   background: transparent;
-  box-shadow:none;
+  box-shadow: none;
   cursor: pointer;
-  color:#ddd;
+  color: #ddd;
+}
+::v-deep .no_border .el-textarea__inner{
+  border: none;
+  box-shadow:none;
 }
 </style>
---
+++ src/views/kanban/m-dialog.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+let dialogVisible = ref(false);
+function handleClose(){
+    console.log(`close`);
+    
+}
+defineExpose({dialogVisible})
+</script>
+
+<template>
+    <div>
+        <el-dialog
+    v-model="dialogVisible"
+    title="Tips"
+    width="30%"
+    :before-close="handleClose"
+  >
+    <span>This is a message</span>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="dialogVisible = false">
+          Confirm
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
+    </div>
+</template>
